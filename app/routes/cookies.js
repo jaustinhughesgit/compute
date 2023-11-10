5e3e5ec94782507b28040884f26a828f4a470099
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-10 13:50:39 -0500

    cookies does text2.txt instad of /*

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -42,7 +42,7 @@ router.get('/', async function(req, res, next) {
     const policy = JSON.stringify({
         Statement: [
             {
-                Resource: 'https://public.1var.com/*', // The URL pattern to allow
+                Resource: 'https://public.1var.com/test2.txt', // The URL pattern to allow
                 Condition: {
                     DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + twoMinutes) / 1000) }
                 }
