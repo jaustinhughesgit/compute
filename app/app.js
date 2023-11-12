229650243cffdd0d1d25bde87cbdce42bcfcca48
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-12 09:32:41 -0500

    logging

--- app/app.js
+++ app/app.js
@@ -10,21 +10,22 @@ app.use(express.urlencoded({ extended: true }));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-console.log("1");
+console.log("start");
 const client = new SecretsManagerClient({ region: "us-east-1" });
 
 async function retrieveSecret() {
     try {
-        console.log("before", client)
+        console.log("client", client)
         const command = await new GetSecretValueCommand({ SecretId: "public/1var/s3" });
+        console.log("command", command)
         const response = await client.send(command);
-        console.log("after", response)
+        console.log("response", response)
         const secretString = response.SecretString;
-        console.log("3");
+        console.log("secretString", secretString);
         const secret = JSON.parse(secretString);
-        console.log("7");
+        console.log("secret",secret);
         privateKey = secret.privateKey; // Adjust according to your secret's structure
-        console.log(privateKey);
+        console.log("privateKey", privateKey);
         // Setup your routes here
         var indexRouter = require('./routes/index');
         var cookiesRouter = require('./routes/cookies')(privateKey); // Pass privateKey to your router
