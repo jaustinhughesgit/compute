230325249974c336e194372d4ba57afb307eefba
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-11 19:17:16 -0500

    openai version of setupRoutes

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 const AWS = require('aws-sdk');
 const secretsManager = new AWS.SecretsManager();
-let privateKey;
 
 async function getSecret() {
     const secretName = 'public/1var/s3'; // Replace with your secret name
@@ -16,72 +15,47 @@ async function getSecret() {
     }
 }
 
-await (async () => {
+async function setupRoutes() {
     try {
         const secret = await getSecret();
-        privateKey = JSON.parse(secret).privateKey; // Ensure this matches how you've stored the key
+        const privateKey = JSON.parse(secret).privateKey; // Ensure this matches how you've stored the key
+
+        // Your CloudFront key pair ID
+        const keyPairId = 'K2LZRHRSYZRU3Y'; // Replace with your key pair ID
+
+        // Create a CloudFront signer using the retrieved private key
+        const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
+
+        router.get('/', async function(req, res, next) {
+            const twoMinutes = 30000; // .5 minutes in milliseconds
+            const policy = JSON.stringify({
+                Statement: [
+                    {
+                        Resource: 'https://public.1var.com/test.txt', // The URL pattern to allow
+                        Condition: {
+                            DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + twoMinutes) / 1000) }
+                        }
+                    }
+                ]
+            });
+
+            const cookies = signer.getSignedCookie({
+                policy: policy
+            });
+
+            for (const cookieName in cookies) {
+                res.cookie(cookieName, cookies[cookieName], { maxAge: twoMinutes, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
+            }
+
+            res.render('cookies', { title: 'Test' });
+        });
+
     } catch (err) {
         console.error(err);
+        // Handle error appropriately
     }
-})();
+}
 
-
-
-// Your CloudFront key pair ID and private key
-const keyPairId = 'K2LZRHRSYZRU3Y'; // Replace with your key pair ID 123
-//const privateKey = secret; // Replace with your private key 123
-
-// Use this code snippet in your app.
-// If you need more information about configurations or implementing the sample code, visit the AWS docs:
-// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
-
-
-  
-
-  
-  // Your code goes here
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Create a CloudFront signer
-const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
-
-router.get('/', async function(req, res, next) {
-    // Set the policy for the signed cookies
-    const twoMinutes = 30000; // .5 minutes in milliseconds
-    const policy = JSON.stringify({
-        Statement: [
-            {
-                Resource: 'https://public.1var.com/test.txt', // The URL pattern to allow
-                Condition: {
-                    DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + twoMinutes) / 1000) }
-                }
-            }
-        ]
-    });
-
-    // Generate signed cookies
-    const cookies = signer.getSignedCookie({
-        policy: policy
-    });
-
-    // Set the signed cookies in the response
-    for (const cookieName in cookies) {
-        res.cookie(cookieName, cookies[cookieName], { maxAge: twoMinutes, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
-    }
-
-    res.render('cookies', { title: 'Test' });
-});
+setupRoutes();
 
 module.exports = router;
