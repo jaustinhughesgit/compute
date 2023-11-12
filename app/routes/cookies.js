7abd73566ce8a3471fe07358ee7753e0321ab3be
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-11 19:09:40 -0500

    await to get secret

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -16,12 +16,15 @@ async function getSecret() {
     }
 }
 
-(async () => {
+await (async () => {
     try {
         const secret = await getSecret();
         privateKey = JSON.parse(secret).privateKey; // Ensure this matches how you've stored the key
+    } catch (err) {
+        console.error(err);
+    }
+})();
 
-console.log(privateKey);
 
 
 // Your CloudFront key pair ID and private key
@@ -80,8 +83,5 @@ router.get('/', async function(req, res, next) {
 
     res.render('cookies', { title: 'Test' });
 });
-} catch (err) {
-    console.error(err);
-}
-})();
+
 module.exports = router;
