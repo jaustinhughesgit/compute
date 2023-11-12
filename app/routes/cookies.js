8333c2090784a7c1e4eca00265cde9c89f663e04
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-11 21:18:36 -0500

    render /cookies

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -1,42 +1,44 @@
 var express = require('express');
+var router = express.Router();
 const AWS = require('aws-sdk');
 
-// Export a function that accepts privateKey
 module.exports = function(privateKey) {
     var router = express.Router();
 
-    // Your CloudFront key pair ID
-    const keyPairId = 'K2LZRHRSYZRU3Y'; // Replace with your key pair ID
+// Your CloudFront key pair ID and private key
+const keyPairId = 'K2LZRHRSYZRU3Y'; // Replace with your key pair ID 123
 
-    // Create a CloudFront signer using the privateKey passed from app.js
-    const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
 
-    router.get('/', async function(req, res, next) {
-        // Set the policy for the signed cookies
-        const twoMinutes = 30000; // .5 minutes in milliseconds
-        const policy = JSON.stringify({
-            Statement: [
-                {
-                    Resource: 'https://public.1var.com/test.txt', // The URL pattern to allow
-                    Condition: {
-                        DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + twoMinutes) / 1000) }
-                    }
+// Create a CloudFront signer
+const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
+
+router.get('/', async function(req, res, next) {
+    // Set the policy for the signed cookies
+    const twoMinutes = 30000; // .5 minutes in milliseconds
+    const policy = JSON.stringify({
+        Statement: [
+            {
+                Resource: 'https://public.1var.com/test.txt', // The URL pattern to allow
+                Condition: {
+                    DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + twoMinutes) / 1000) }
                 }
-            ]
-        });
+            }
+        ]
+    });
 
-        // Generate signed cookies
-        const cookies = signer.getSignedCookie({
-            policy: policy
-        });
+    // Generate signed cookies
+    const cookies = signer.getSignedCookie({
+        policy: policy
+    });
 
-        // Set the signed cookies in the response
-        for (const cookieName in cookies) {
-            res.cookie(cookieName, cookies[cookieName], { maxAge: twoMinutes, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
-        }
-
+    // Set the signed cookies in the response
+    for (const cookieName in cookies) {
+        res.cookie(cookieName, cookies[cookieName], { maxAge: twoMinutes, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
+    }
+        // Render the 'cookies' view with the title 'Test'
         res.render('cookies', { title: 'Test' });
     });
 
     return router;
-};
+
+}
