7a284126f01ca6c11a56f6bbcf56048cbe36ecb3
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-11 19:44:04 -0500

    Reverting back to inline key

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -1,57 +1,86 @@
-const express = require('express');
-const router = express.Router();
-const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+var express = require('express');
+var router = express.Router();
+const AWS = require('aws-sdk');
 
-const secretName = 'public/1var/s3'; // Replace with your secret name
-const region = 'us-east-1'; // Replace with your region
-const client = new SecretsManagerClient({ region });
 
-let privateKey;
-let isSecretFetched = false;
+// Your CloudFront key pair ID and private key
+const keyPairId = 'K2LZRHRSYZRU3Y'; // Replace with your key pair ID 123
+const privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIIEowIBAAKCAQEAyqcY3cxlKen6cQhkdMdY6rmT8NmKOsC1gC3pdj37jowCKa0u
+UV5uyotiGDOOKAcuT6GA7btPUtJ+sexhFeZFCWOA+uHP+LqHCyRFieozkBWZm5PP
+o3iTuz7rQXBLNokC1o41cPHmRFtFWRrp5lsiZx61pFdQ6XPBACuHcTZK7cMo24Xt
+wSnhzXXwy/jnd4p+X6VsoWJy8EaHupR00cs6JcLmOTVPJrddJFVV29DZ9Ht88Wxq
+Z7+C+Qr8soOJz/D7bzJHcwDUEoDp75vjByfjTpS0Q5bvWD8XXAiuDOmR8sQa9+Cf
+Ph4mykvTDiWPiSiqc9rt8v2lJLiizIu8HwaOZQIDAQABAoIBAAM5xVcOdakaTNQM
+w/s/NbvyvUHvAdjCkCux+g6EQ7ihTjgsm1lMjVXoVw+Mjog2PmH3xPjDwP+lUxfw
+68YF9suvS50dXV/s/tD0wFicI5BOZQtevxuFLqYjL7/IZ6IUFzlOuoIJj6vp31Y2
+QUqF4SvOsinOAVSIn6X+4LaIHImL851RqtMpsud/RdKQi4ZArYt1Sg0278i66qcg
+gwNwND+35TYwAOtLBid3855BjA9jVE9eo7GzNmETeXl7ijcAEGNOJoA2dqueqOxA
+2lWhC8TbifmK0OSacVj4X2LDrPI8CzJq2SYHIpVf+MpuV2+FfQF6ITY0/OUjnfuY
+l8elJMcCgYEA69JVau7+XHFQ6IQRs1G0YMUW5kZ6uuW/1JbooQdtTT2kExi1so+P
+kQ5KdqIbDix1FxopHCDIApDVjMr/Yq+vZx5XlOPTS9Es5HNHhyheS7ICKC5mjRqD
+cRxXwrNUIAwjFFIMeiKrYV6tB/PNMur5p4YXeyEsUVyNv2pfFrDxEb8CgYEA2/4z
+ImCAAZHDchdN4m+ndM/5MUekw524i8gikdRLGxpks3L77MP/PHvxKi/k6xYsWKmr
+q3LLePlcm6NfFsF19dCzpMSvbANSqUZlJc2t4uSv9JCuDD71nktgLMyKrVotVWRV
+vo7CvxQJlh5xlu+DgaYcxWXpmPSVSHBOv02LoNsCgYA6Ryyc1Js/tTFNhPXq3tI4
+5/wUxG4oKgcSPokW4oL8h7M4lO6yRhAwxNtaHg2ZnxsArpJiRSeomqprtO8QMGKk
+lTcHsJXTMsppWqPenvdOtZsa2vy0+kxpc5usniy4DsfMicpTlKXN1lvkjzey0acI
+43aCE1ykbr8JAvyk2u14eQKBgQCLR4DwPtBMLhDjZsW0mqQWXKWUAZvbDTwNo4Pf
+d9ylKCyhQCcnW194169z2ibAf6VL7P/26BLSYG21S9Wj/o/ENYHGy4+UfvYSnbLk
+IDf68nZEDGVk82dl9KrLMiSKZBFXgtKWdqPtfa4kENoxiSplJtoIT+F5KUBqQFBa
+5amFCQKBgG3BYOqWK6OvyHtlXtU42t7p4rHn/UxBcSC3HAIqaG+0uRIulINMXgdY
+lTKQv68uE/g5Q/TJakS84cH8+Y1MsONSO/7ePfEkhYKge9esPqm4T01pbTFnLQTy
+pRleuzod+Uyss2+pgG2FiI/DjbWBIBE9Pn2zhPiCchUE7DCMb341
+-----END RSA PRIVATE KEY-----`; // Replace with your private key 123
 
-async function fetchSecret() {
-    try {
-        const response = await client.send(new GetSecretValueCommand({ SecretId: secretName }));
-        if (response.SecretString) {
-            privateKey = JSON.parse(response.SecretString).privateKey; // Adjust based on your secret's structure
-            console.log("1",privateKey);
-        } else {
-            let buff = Buffer.from(response.SecretBinary, 'base64');
-            privateKey = buff.toString('ascii');
-            console.log("2", privateKey);
-        }
-        isSecretFetched = true;
-    } catch (error) {
-        console.error("Error fetching secret:", error);
-        throw error;
-    }
-}
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/getting-started.html
 
-// Fetch the secret when the module is loaded
-fetchSecret().catch(console.error);
 
-// Middleware to ensure secret is loaded
-function ensureSecretLoaded(req, res, next) {
-    if (!isSecretFetched) {
-        return res.status(503).send('Service not ready');
-    }
-    next();
-}
+  
+  const secret_name = "public/1var/s3";
+  
+  const client = new SecretsManagerClient({
+    region: "us-east-1",
+  });
+  
+  let response;
+  
+  try {
+    response = await client.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT", // VersionStage defaults to AWSCURRENT if unspecified
+      })
+    );
+  } catch (error) {
+    // For a list of exceptions thrown, see
+    // https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+    throw error;
+  }
+  
+  const secret = response.SecretString;
+  
+  // Your code goes here
 
-router.use(ensureSecretLoaded);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Create a CloudFront signer
+const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
 
 router.get('/', async function(req, res, next) {
-    if (!privateKey) {
-        return res.status(500).send('Private key not loaded');
-    }
-
-    // Your CloudFront key pair ID
-    const keyPairId = 'K2LZRHRSYZRU3Y'; // Replace with your key pair ID
-
-    // Create a CloudFront signer using the retrieved private key
-    const AWS = require('aws-sdk');
-    const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
-
     // Set the policy for the signed cookies
     const twoMinutes = 30000; // .5 minutes in milliseconds
     const policy = JSON.stringify({
