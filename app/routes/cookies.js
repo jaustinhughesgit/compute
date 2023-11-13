47a3a39ea0326f12884346d019244e3750d68879
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-13 11:41:44 -0500

    Moving secrets manager to cookies.js

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -1,44 +1,62 @@
 var express = require('express');
-var router = express.Router();
 const AWS = require('aws-sdk');
 
-module.exports = function(privateKey) {
-    var router = express.Router();
+// Initialize AWS Secrets Manager
+const SM = new AWS.SecretsManager();
 
-// Your CloudFront key pair ID and private key
-const keyPairId = 'K2LZRHRSYZRU3Y'; // Replace with your key pair ID 123
+async function getPrivateKey() {
+    const secretName = "public/1var/s3"; // Replace with your secret name
+    try {
+        const data = await SM.getSecretValue({ SecretId: secretName }).promise();
+        const secret = JSON.parse(data.SecretString);
+        return secret.privateKey;
+    } catch (error) {
+        console.error("Error fetching secret:", error);
+        throw error;
+    }
+}
 
-
-// Create a CloudFront signer
-const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
+var router = express.Router();
 
 router.get('/', async function(req, res, next) {
-    // Set the policy for the signed cookies
-    const twoMinutes = 30000; // .5 minutes in milliseconds
-    const policy = JSON.stringify({
-        Statement: [
-            {
-                Resource: 'https://public.1var.com/test.txt', // The URL pattern to allow
-                Condition: {
-                    DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + twoMinutes) / 1000) }
+    try {
+        const privateKey = await getPrivateKey();
+
+        // Your CloudFront key pair ID and private key
+        const keyPairId = 'K2LZRHRSYZRU3Y'; // Replace with your key pair ID
+
+        // Create a CloudFront signer
+        const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
+
+        // Set the policy for the signed cookies
+        const twoMinutes = 30000; // .5 minutes in milliseconds
+        const policy = JSON.stringify({
+            Statement: [
+                {
+                    Resource: 'https://public.1var.com/test.txt', // The URL pattern to allow
+                    Condition: {
+                        DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + twoMinutes) / 1000) }
+                    }
                 }
-            }
-        ]
-    });
+            ]
+        });
 
-    // Generate signed cookies
-    const cookies = signer.getSignedCookie({
-        policy: policy
-    });
+        // Generate signed cookies
+        const cookies = signer.getSignedCookie({
+            policy: policy
+        });
 
-    // Set the signed cookies in the response
-    for (const cookieName in cookies) {
-        res.cookie(cookieName, cookies[cookieName], { maxAge: twoMinutes, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
-    }
+        // Set the signed cookies in the response
+        for (const cookieName in cookies) {
+            res.cookie(cookieName, cookies[cookieName], { maxAge: twoMinutes, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
+        }
+
         // Render the 'cookies' view with the title 'Test'
         res.render('cookies', { title: 'Test' });
+    } catch (error) {
+        console.error("Error in /cookies route:", error);
+        res.status(500).send("Server Error");
+    }
 });
 
-return router;
-
-}
+module.exports = router;
