7e93ea6fc4527d55d3e78689fafe1f012950d396
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-13 11:50:53 -0500

    previous code with inline key

--- app/app.js
+++ app/app.js
@@ -1,6 +1,22 @@
+const AWS = require('aws-sdk');
 const express = require('express');
 const serverless = require('serverless-http');
 const path = require('path');
+
+// Initialize AWS Secrets Manager
+const SM = new AWS.SecretsManager();
+
+async function getPrivateKey() {
+    const secretName = "public/1var/s3"; // Replace with your secret name
+    try {
+        const data = await SM.getSecretValue({ SecretId: secretName }).promise();
+        const secret = JSON.parse(data.SecretString);
+        return secret.privateKey;
+    } catch (error) {
+        console.error("Error fetching secret:", error);
+        throw error;
+    }
+}
 
 const app = express();
 
@@ -10,9 +26,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 var indexRouter = require('./routes/index');
-var cookiesRouter = require('./routes/cookies');
+var cookiesRouter;
+
+// Middleware to ensure privateKey is loaded
+app.use(async (req, res, next) => {
+    if (!cookiesRouter) {
+        try {
+            const privateKey = await getPrivateKey();
+            cookiesRouter = require('./routes/cookies')(privateKey);
+            app.use('/cookies', cookiesRouter);
+            next();
+        } catch (error) {
+            console.error("Failed to retrieve private key:", error);
+            res.status(500).send("Server Error");
+        }
+    } else {
+        next();
+    }
+});
 
 app.use('/', indexRouter);
-app.use('/cookies', cookiesRouter);
 
 module.exports.lambdaHandler = serverless(app);
