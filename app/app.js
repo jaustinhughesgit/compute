eff90a92b91dc891a138659531d614b21dea8628
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-14 14:19:17 -0500

    added bodyParser json and urlencoded

--- app/app.js
+++ app/app.js
@@ -27,6 +27,8 @@ async function getPrivateKey() {
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true })); 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
