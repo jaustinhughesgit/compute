cd4153ed4c9f6fbd2d7e77022fbfab318bb5b994
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-14 17:12:19 -0500

    versions and words

--- app/routes/controller.js
+++ app/routes/controller.js
@@ -5,6 +5,7 @@ module.exports = (dynamodb, dynamodbLL, uuidv4) => {
     console.log("dynamodb",dynamodb);
     console.log("dynamodbLL",dynamodbLL);
     console.log("uuidv4",uuidv4);
+
     router.get('/', async function(req, res, next) {
         res.render('controller', {title:'controller'})
     });
@@ -88,7 +89,172 @@ module.exports = (dynamodb, dynamodbLL, uuidv4) => {
                 res.render('controller', {results: JSON.stringify(data)});
             }
         });
+    });
+
+    router.post('/createCounterW', function(req, res) {
+        const tableParams = {
+            AttributeDefinitions: [
+                {
+                    AttributeName: 'pk',
+                    AttributeType: 'S'
+                }
+            ],
+            KeySchema: [
+                {
+                    AttributeName: 'pk',
+                    KeyType: 'HASH'
+                }
+            ],
+            BillingMode: 'PAY_PER_REQUEST',  // You're using on-demand capacity, so you don't specify ProvisionedThroughput
+            TableName: 'wCounter'
+        };
+
+        dynamodbLL.createTable(tableParams, (err, data) => {
+            if (err) {
+                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+                return res.status(500).send(err); // You might want to handle error differently
+            } else {
+                console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+                
+                
+                        res.render('controller', {results: JSON.stringify({data})});
+                    
+            }
         });
+        
+    });
+
+    router.post('/createWordsTable', function(req, res) {
+        const tableParams = {
+            AttributeDefinitions: [
+                {
+                    AttributeName: 'a',
+                    AttributeType: 'N'
+                },
+                {
+                    AttributeName: 's',
+                    AttributeType: 'S'
+                }
+            ],
+            KeySchema: [
+                {
+                    AttributeName: 'a',
+                    KeyType: 'HASH'
+                }
+            ],
+            GlobalSecondaryIndexes: [ 
+                {
+                    IndexName: 'sIndex',
+                    KeySchema: [
+                        {
+                            AttributeName: 's',
+                            KeyType: 'HASH'
+                        }
+                    ],
+                    Projection: {
+                        ProjectionType: 'ALL'
+                    }
+                }
+            ],
+            BillingMode: 'PAY_PER_REQUEST',  // You're using on-demand capacity, so you don't specify ProvisionedThroughput
+            TableName: 'words'
+        };
+
+        dynamodbLL.createTable(tableParams, (err, data) => {
+            if (err) {
+                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+                return res.status(500).send(err); // You might want to handle error differently
+            } else {
+                console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+                res.render('controller', {results: JSON.stringify(data)});
+            }
+        });
+    });
+
+    router.post('/createCounterV', function(req, res) {
+        const tableParams = {
+            AttributeDefinitions: [
+                {
+                    AttributeName: 'pk',
+                    AttributeType: 'S'
+                }
+            ],
+            KeySchema: [
+                {
+                    AttributeName: 'pk',
+                    KeyType: 'HASH'
+                }
+            ],
+            BillingMode: 'PAY_PER_REQUEST',  // You're using on-demand capacity, so you don't specify ProvisionedThroughput
+            TableName: 'vCounter'
+        };
+
+        dynamodbLL.createTable(tableParams, (err, data) => {
+            if (err) {
+                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+                return res.status(500).send(err); // You might want to handle error differently
+            } else {
+                console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+                
+                
+                        res.render('controller', {results: JSON.stringify({data})});
+                    
+            }
+        });
+        
+    });
+
+    router.post('/createVersionTable', function(req, res) {
+        const tableParams = {
+            AttributeDefinitions: [
+                {
+                    AttributeName: 'v',
+                    AttributeType: 'S'
+                },
+                {
+                    AttributeName: 'e',
+                    AttributeType: 'S'
+                }
+            ],
+            KeySchema: [
+                {
+                    AttributeName: 'v',
+                    KeyType: 'HASH'
+                }
+            ],
+            GlobalSecondaryIndexes: [ 
+                {
+                    IndexName: 'eIndex',
+                    KeySchema: [
+                        {
+                            AttributeName: 'e',
+                            KeyType: 'HASH'
+                        }
+                    ],
+                    Projection: {
+                        ProjectionType: 'ALL'
+                    }
+                }
+            ],
+            BillingMode: 'PAY_PER_REQUEST',  // You're using on-demand capacity, so you don't specify ProvisionedThroughput
+            TableName: 'versions'
+        };
+
+        dynamodbLL.createTable(tableParams, (err, data) => {
+            if (err) {
+                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+                return res.status(500).send(err); // You might want to handle error differently
+            } else {
+                console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+                res.render('controller', {results: JSON.stringify(data)});
+            }
+        });
+    });
+
+
+
+
+
 
     return router;
 };
