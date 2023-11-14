7cacbc28e2c7de288f995c533f1c9bdf15b174cd
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-14 10:45:53 -0500

    url false

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -5,44 +5,43 @@ const AWS = require('aws-sdk');
 module.exports = function(privateKey) {
     var router = express.Router();
 
-const keyPairId = 'K2LZRHRSYZRU3Y'; 
+    const keyPairId = 'K2LZRHRSYZRU3Y';
+    const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
 
-const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
-
-router.get('/', async function(req, res, next) {
-    const expires = 30000; // .5 minutes in milliseconds
-    const url = "https://public.1var.com/test2.txt";
-    const policy = JSON.stringify({
-        Statement: [
-            {
-                Resource: url,
-                Condition: {
-                    DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + expires) / 1000) }
+    router.get('/', async function(req, res, next) {
+        const expires = 30000; // .5 minutes in milliseconds
+        const url = "https://public.1var.com/test2.txt";
+        const policy = JSON.stringify({
+            Statement: [
+                {
+                    Resource: url,
+                    Condition: {
+                        DateLessThan: { 'AWS:EpochTime': Math.floor((Date.now() + expires) / 1000) }
+                    }
                 }
+            ]
+        });
+
+        // Check if the request accepts JSON (RESTful API request)
+        if (false) {
+            const url = signer.getSignedUrl({
+                url: 'https://public.1var.com/test.txt',
+                policy: policy
+            });
+            res.json({ signedUrl: url });
+        } else {
+            // Handle as a web request, set cookies
+            const cookies = signer.getSignedCookie({
+                policy: policy
+            });
+
+            for (const cookieName in cookies) {
+                res.cookie(cookieName, cookies[cookieName], { maxAge: twoMinutes, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
             }
-        ]
+            res.render('cookies', { title: 'Test' });
+        }
     });
 
-    // Check if the request accepts JSON (RESTful API request)
-    if (req.accepts('json')) {
-        const url = signer.getSignedUrl({
-            url: 'https://public.1var.com/test.txt',
-            policy: policy
-        });
-        res.json({ signedUrl: url });
-    } else {
-        // Handle as a web request, set cookies
-        const cookies = signer.getSignedCookie({
-            policy: policy
-        });
-
-        for (const cookieName in cookies) {
-            res.cookie(cookieName, cookies[cookieName], { maxAge: twoMinutes, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
-        }
-        res.render('cookies', { title: 'Test' });
-    }
-});
-
-return router;
+    return router;
 
 }
