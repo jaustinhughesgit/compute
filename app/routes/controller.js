ebc3a88db7561077795ab10f5c025ce870c64387
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-14 14:10:07 -0500

    create entity counter and table

--- app/routes/controller.js
+++ app/routes/controller.js
@@ -9,5 +9,83 @@ module.exports = (dynamodb, dynamodbLL, uuidv4) => {
         res.render('controller', {title:'controller'})
     });
     
+    router.post('/createCounterE', function(req, res) {
+        const tableParams = {
+            AttributeDefinitions: [
+                {
+                    AttributeName: 'pk',
+                    AttributeType: 'S'
+                }
+            ],
+            KeySchema: [
+                {
+                    AttributeName: 'pk',
+                    KeyType: 'HASH'
+                }
+            ],
+            BillingMode: 'PAY_PER_REQUEST',  // You're using on-demand capacity, so you don't specify ProvisionedThroughput
+            TableName: 'eCounter'
+        };
+
+        dynamodbLL.createTable(tableParams, (err, data) => {
+            if (err) {
+                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+                return res.status(500).send(err); // You might want to handle error differently
+            } else {
+                console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+                        res.render('setupdb', {results: JSON.stringify({data})});
+                   
+            }
+        });
+        
+    });
+
+    router.post('/createEntityTable', function(req, res) {
+        const tableParams = {
+            AttributeDefinitions: [
+                {
+                    AttributeName: 'e',
+                    AttributeType: 'S'
+                },
+                {
+                    AttributeName: 'v',
+                    AttributeType: 'S'
+                }
+            ],
+            KeySchema: [
+                {
+                    AttributeName: 'e',
+                    KeyType: 'HASH'
+                }
+            ],
+            GlobalSecondaryIndexes: [ 
+                {
+                    IndexName: 'vIndex',
+                    KeySchema: [
+                        {
+                            AttributeName: 'v',
+                            KeyType: 'HASH'
+                        }
+                    ],
+                    Projection: {
+                        ProjectionType: 'ALL'
+                    }
+                }
+            ],
+            BillingMode: 'PAY_PER_REQUEST',  // You're using on-demand capacity, so you don't specify ProvisionedThroughput
+            TableName: 'entities'
+        };
+    
+        dynamodbLL.createTable(tableParams, (err, data) => {
+            if (err) {
+                console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
+                return res.status(500).send(err); // You might want to handle error differently
+            } else {
+                console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
+                res.render('setupdb', {results: JSON.stringify(data)});
+            }
+        });
+        });
+
     return router;
 };
