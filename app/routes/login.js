eece57fb98f86e5e1f13614a14c18d6157019646
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-21 13:04:20 -0500

    debug log

--- app/routes/login.js
+++ app/routes/login.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 router.get('/', async function(req, res, next){
     const debug = require('debug')
+    debug('Login page accessed');
     res.render('login', {title:'1 Var', message:JSON.stringify(debug)})
 });
 
