91cce0ae3817766513d3690a6f4b444ad2c8f252
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-21 13:22:40 -0500

    passport-custom

--- app/routes/github.js
+++ app/routes/github.js
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
             owner: 'jaustinhughesgit',
             repo: 'compute',
             event_type: 'install-npm-package',
-            client_payload: { packageName: "debug" } 
+            client_payload: { packageName: "passport-custom" } 
         });
 
         res.render('github', { message: "GitHub Actions workflow triggered successfully.", error: null });
