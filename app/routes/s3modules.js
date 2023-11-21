8a1f96729c2ed717a7a8d7efe64b2c4eeeef271f
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-21 15:57:36 -0500

    no zip s3 node_modules

--- app/routes/s3modules.js
+++ app/routes/s3modules.js
@@ -1,41 +1,50 @@
 const AWS = require('aws-sdk');
-const { execSync } = require('child_process');
 const fs = require('fs');
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 const s3 = new AWS.S3();
 
-router.get('/', async function(req, res, next){
+router.get('/', async function(req, res, next) {
     try {
-        await downloadAndUnzipNodeModules('new_modules.zip'); // Replace with the correct key if needed
+        // Example: Download and load the 'debug' module
+        await downloadAndPrepareModule('debug');
         const debug = require('debug')('myapp');
         debug('Debug module is working!');
-        res.render('s3modules', {title: 's3 modules', message: 'Debug module loaded successfully!'});
+        res.render('s3modules', { title: 's3 modules', message: 'Debug module loaded successfully!' });
     } catch (error) {
         console.error('Error:', error);
-        res.render('s3modules', {title: 's3 modules', error: 'Failed to load module.'});
+        res.render('s3modules', { title: 's3 modules', error: 'Failed to load module.' });
     }
 });
 
-async function downloadAndUnzipNodeModules(key) {
+async function downloadAndPrepareModule(moduleName) {
+    const modulePath = `/tmp/node_modules/${moduleName}`;
+    if (!fs.existsSync(modulePath)) {
+        // The module is not in the cache, download it
+        await downloadModuleFromS3(moduleName, modulePath);
+    }
+    // Add the module to the NODE_PATH
+    process.env.NODE_PATH = `/tmp/node_modules`;
+    require('module').Module._initPaths();
+}
+
+async function downloadModuleFromS3(moduleName, modulePath) {
     const params = {
         Bucket: "1var-node-modules",
-        Key: key,
+        Key: `node_modules/${moduleName}`,
+        // If your modules are stored in a nested structure, adjust the Key accordingly
     };
 
-    // Download the zip file from S3
-    const zipFile = await s3.getObject(params).promise();
-
-    // Write the zip file to the /tmp directory
-    fs.writeFileSync(`/tmp/${key}`, zipFile.Body);
-
-    // Unzip the file
-    execSync(`unzip -o /tmp/${key} -d /tmp/`, { stdio: 'inherit' });
-
-    // Set NODE_PATH to use /tmp/node_modules
-    process.env.NODE_PATH = '/tmp/node_modules';
-    require('module').Module._initPaths();
+    try {
+        const data = await s3.getObject(params).promise();
+        fs.mkdirSync(path.dirname(modulePath), { recursive: true });
+        fs.writeFileSync(modulePath, data.Body);
+    } catch (error) {
+        console.error(`Error downloading module ${moduleName}:`, error);
+        throw error;
+    }
 }
 
 module.exports = router;
