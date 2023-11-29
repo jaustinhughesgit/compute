26ba855dcae719e899dc5c315eb94aa368588cff
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-29 13:32:23 -0500

    added execution to all

--- app/routes/dynode8.js
+++ app/routes/dynode8.js
@@ -20,7 +20,7 @@ const json = {
                 { "method": "tz", "params": ["Asia/Dubai"] },
                 { "method": "format", "params": ["YYYY-MM-DD HH:mm:ss"] }
             ],
-            "assignTo": "{{timeInDubai}}"
+            "assignTo": "{{timeInDubai}}!"
         },
         {
             "module": "moment-timezone",
@@ -33,7 +33,7 @@ const json = {
         {
             "module": "moment-timezone",
             "assignTo": "timeInDubai2",
-            "valueFrom": ["{{timeInDubai}}"],
+            "valueFrom": ["{{timeInDubai}}!"],
             "chain": [
                 { "method": "add", "params": [1, "hours"] },
                 { "method": "format", "params": ["YYYY-MM-DD HH:mm:ss"] }
