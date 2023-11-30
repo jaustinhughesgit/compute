8e23227870b15db76eeaddf8bc71c355587fe7a4
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-30 01:14:03 -0500

    no chain Strategy

--- app/routes/dynode9.js
+++ app/routes/dynode9.js
@@ -171,7 +171,7 @@ const json = {
         {
             "module":"passport-microsoft",
             "chain":[
-               {"method":"Strategy", "params":[]}
+               
             ],
             "assignTo":"passportmicrosoft"
         }
@@ -204,7 +204,7 @@ local.dyRouter.all('/*', async function(req, res, next) {
 
     await initializeModules(context, json, req, res, next);
     //if (context.authenticateMicrosoft) {
-        context.passport.use(new context.passportmicrosoft({
+        context.passport.use(new context.passportmicrosoft.Strategy({
             "clientID": process.env.MICROSOFT_CLIENT_ID,
             "clientSecret": process.env.MICROSOFT_CLIENT_SECRET,
             "callbackURL": "https://compute.1var.com/auth/microsoft/callback",
