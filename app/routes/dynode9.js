985d5d0cdc0aee4156acfaf078c3d78da45d18d9
jaustinhughesgit <jaustinhughes@outlook.com>
2023-11-30 15:27:44 -0500

    adding {{}}! to newStrategy

--- app/routes/dynode9.js
+++ app/routes/dynode9.js
@@ -55,7 +55,7 @@ const json = {
                     "microsoft", "{{passportmicrosoft}}!"
                 ]}
             ],
-            "assignTo":"newStrategy"
+            "assignTo":"{{newStrategy}}!"
         }
     ]
 }
