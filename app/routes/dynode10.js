7084b6ce6eaa4c3de637a02c928dd4870edad1f7
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-04 13:45:28 -0500

    temp moving urlpath to middleware

--- app/routes/dynode10.js
+++ app/routes/dynode10.js
@@ -171,6 +171,8 @@ const json = [
 let middlewareFunctions = json.map(stepConfig => {
     return async (req, res, next) => {
         local.context = await processConfig(stepConfig);
+        local.context["urlpath"] = req.path
+        local.context["strategy"] = req.path.startsWith('/auth') ? req.path.split("/")[2] : "";
         await initializeModules(local.context, stepConfig, req, res, next);
     };
 });
@@ -179,8 +181,6 @@ local.dyRouter.all('/*', async function(req, res, next) {
     local.req = req;
     local.res = res;
     local.console = console;
-    local.context["urlpath"] = req.path
-    local.context["strategy"] = req.path.startsWith('/auth') ? req.path.split("/")[2] : "";
 }, ...middlewareFunctions, async (req, res, next) => {
     console.log("done")
 });
