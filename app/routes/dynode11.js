fd8080e7ac960ef271df205de292a229d1de11e5
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-06 11:52:46 -0500

    1111

--- app/routes/dynode11.js
+++ app/routes/dynode11.js
@@ -400,6 +400,7 @@ async function initializeModules(context, config, req, res, next) {
                 console.log("action",action)
                 if (action.module.startsWith("{{")){
                     console.log("<-- context")
+                    console.log(action.module)
                     moduleInstance = replacePlaceholders(action.module, context) //context[action.module.replace("{{","").replace("}}","")] //<-----/////
                  } else if (local[action.module]){
                     console.log("<-- local")
