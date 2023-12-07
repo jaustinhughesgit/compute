8cc0066791685d1036cbefa62abaf58931fc6257
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-07 04:52:42 -0500

    miro code

--- app/routes/miro.js
+++ app/routes/miro.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+const {Miro} = require('@mirohq/miro-api')
+const session = require('express-session')
+const miro = new Miro()
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  }),
+)
 
 router.get('/', async function(req, res, next){
-    res.render('miro', {response:'...'})
+    if (!(await miro.isAuthorized(req.session.id))) {
+      res.redirect(miro.getAuthUrl())
+      return
+    }
+  
+    res.contentType('html')
+    res.write('List of boards available to the team:')
+    res.write('<ul>')
+  
+    const api = miro.as(req.session.id)
+  
+    for await (const board of api.getAllBoards()) {
+      res.write(`<li><a href="${board.viewLink}">${board.name}</a></li>`)
+    }
+    res.write('</ul>')
+    res.send()
 });
 
+
+app.get('/auth/miro/callback', async (req, res) => {
+    await miro.exchangeCodeForAccessToken(req.session.id, req.query.code)
+    res.redirect('/')
+  })
 
 module.exports = router;
