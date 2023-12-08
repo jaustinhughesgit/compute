799637ab85366ca8eb0c99515d03bbd5b8854c4e
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-08 13:16:23 -0500

    asdf

--- app/routes/dynode13.js
+++ app/routes/dynode13.js
@@ -463,19 +463,16 @@ async function processAction(action, context, req, res, next) {
         if (action.assign.includes('{{')) {
             let isFunctionExecution = action.assign.endsWith('!');
             let assignKey = isFunctionExecution ? action.assign.slice(2, -3) : action.assign.slice(2, -2);
+            action.assign = action.assign.replace("!","")
             let result = createFunctionFromAction(action, context, req, res, next)
             if (isFunctionExecution) {
-                if (typeof result === 'function'){
-                    let tempFunction = () => result;
-                    context[assignKey] = tempFunction();
-                } else {
-                    context[assignKey] = result
-                }
+                context[assignKey] = typeof result === 'function' ? result() : result;
             } else {
                 context[assignKey] = result;
             }
-            
-        } else {
+        }
+        
+        else {
             context[action.assign] = createFunctionFromAction(action, context, req, res, next)
         }
     } 
