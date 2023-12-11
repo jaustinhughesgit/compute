e1ab1c41f211d2dc6d9c8f3c4db87f01a5a04e33
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-11 17:23:20 -0500

    dfadfaaaaaa

--- app/routes/embeddings.js
+++ app/routes/embeddings.js
@@ -9,9 +9,9 @@ router.get('/', async function(req, res, next) {
 
     const chatCompletion = await openai.chat.completions.create({
         messages: [{ role: "user", content: "Say this is a test" }],
-        model: "gpt-3.5-turbo",
+        model: "text-embedding-ada-002",
     });
-    res.render('embeddings', { embeddings: chatCompletion });
+    res.render('embeddings', { embeddings: JSON.stringify(chatCompletion) });
 });
 
 module.exports = router;
