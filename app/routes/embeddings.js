aae74c09f82879929eddd090f11052a37a6150ed
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-11 17:18:01 -0500

    hhhhhhh

--- app/routes/embeddings.js
+++ app/routes/embeddings.js
@@ -1,22 +1,17 @@
 var express = require('express');
 var router = express.Router();
-
-import OpenAI from "openai";
-
-
+var OpenAI = require("openai").default; // Changed to CommonJS require
 
 router.get('/', async function(req, res, next) {
-	
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
 
-const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "Say this is a test" }],
-    model: "gpt-3.5-turbo",
+    const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: "Say this is a test" }],
+        model: "gpt-3.5-turbo",
+    });
+    res.render('index', { embeddings: chatCompletion });
 });
-	res.render('index', { embeddings: chatCompletion });
-  });
-
 
 module.exports = router;
