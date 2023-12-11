ba334ca90ae24d3fe3a1ba1ea4589f80f1f4df4f
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-11 16:34:33 -0500

    no constructor

--- app/routes/embeddings.js
+++ app/routes/embeddings.js
@@ -1,25 +1,24 @@
 var express = require('express');
 var router = express.Router();
-const { Configuration, OpenAIApi } = require("openai");
+const { OpenAIApi } = require("openai");
 
-
-router.get('/', async function(req, res, next){
-    const configuration = new Configuration({
-        apiKey: process.env.OPENAI_API_KEY, // Set your API key in Lambda's environment variables
+router.get('/', async function(req, res, next) {
+    const openai = new OpenAIApi({
+        apiKey: process.env.OPENAI_API_KEY, // Set your API key in environment variables
     });
-    const openai = await new OpenAIApi(configuration);
-    const response = await openai.createEmbedding({
-        model: "text-embedding-ada-002", 
-        input: "/animals/live/ocean" 
-    });
-    console.log(response)
-    res.render('embeddings', {text:"hello"})
 
+    try {
+        const response = await openai.createEmbedding({
+            model: "text-embedding-ada-002",
+            input: "/animals/live/ocean"
+        });
 
-
-
-
+        console.log(response.data);
+        res.render('embeddings', { embeddings: response.data });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Error processing your request");
+    }
 });
-
 
 module.exports = router;
