af69c34fc3e04a7822bf1dda3e66a85b59677de0
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-12 14:40:11 -0500

    category language

--- app/routes/embeddings.js
+++ app/routes/embeddings.js
@@ -8,10 +8,9 @@ router.get('/', async function(req, res, next) {
     });
 
     const chatCompletion = await openai.embeddings.create({
-        input: "Social Media, Posts, Blog, Articles, Newsletter, Gallery, Comments, Likes, Shares, Followers, Podcasts, Videos, Webinars, Influencer Collaborations, Endorsements",
-        model: "text-embedding-ada-002",
+        input: "Languages, Translation, English, Spanish, Chinese (Mandarin), Hindi, Arabic, Portuguese, Bengali, Russian, Japanese, Punjabi, German, Javanese, Wu (Shanghainese), Malay (including Indonesian), Telugu, Vietnamese, Korean, French, Turkish, Marathi, Tamil, Urdu, Persian, Italian, Gujarati, Polish, Ukrainian, Romanian, Dutch, Greek, Thai, Tagalog (Filipino), Czech, Serbian, Hungarian, Swedish, Danish, Finnish, Slovak, Norwegian, Bulgarian, Croatian, Lithuanian, Slovenian, Latvian, Estonian, Hebrew, Swahili, Armenian, Azerbaijani", model: "text-embedding-ada-002",
     });
-    res.render('embeddings', { category: "social", embeddings: JSON.stringify(chatCompletion) });
+    res.render('embeddings', { category: "language", embeddings: JSON.stringify(chatCompletion) });
 });
 
 module.exports = router;
