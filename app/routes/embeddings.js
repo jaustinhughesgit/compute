afb17fbb1b718cc70f7618d080fc4774e4bbbf9d
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-12 15:29:31 -0500

    knowledge

--- app/routes/embeddings.js
+++ app/routes/embeddings.js
@@ -8,9 +8,9 @@ router.get('/', async function(req, res, next) {
     });
 
     const chatCompletion = await openai.embeddings.create({
-        input: "Educational: Degrees, Teachers, Books, Papers, Engineering, Arts, Social Sciences, Online Platforms, Libraries, Educational Apps, Internships, Scholarships, Career Counseling", model: "text-embedding-ada-002",
+        input: "Knowledge: Science, Chemistry, Biology, Mathematics, History, Cosmology, AI, Robotics, Software Development, Climate Change, Biodiversity, Sustainability, Moral Philosophy, Political Theory, Logic", model: "text-embedding-ada-002",
     });
-    res.render('embeddings', { category: "Educational", embeddings: JSON.stringify(chatCompletion) });
+    res.render('embeddings', { category: "Knowledge", embeddings: JSON.stringify(chatCompletion) });
 });
 
 module.exports = router;
