9b14f49b2f8c655b1baf6c65ed36554852a90283
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-12 15:07:46 -0500

    cricket game

--- app/routes/embeddings.js
+++ app/routes/embeddings.js
@@ -8,9 +8,9 @@ router.get('/', async function(req, res, next) {
     });
 
     const chatCompletion = await openai.embeddings.create({
-        input: "News: Positive, Negative, Local, National, Emergency, Family, Business, Technology, Sports, Entertainment, Science, Editorial, Opinion Pieces, Fact-Checking, International Affairs, Global Economy, World Politics", model: "text-embedding-ada-002",
+        input: "Cricket Game", model: "text-embedding-ada-002",
     });
-    res.render('embeddings', { category: "news", embeddings: JSON.stringify(chatCompletion) });
+    res.render('embeddings', { category: "cricket game", embeddings: JSON.stringify(chatCompletion) });
 });
 
 module.exports = router;
