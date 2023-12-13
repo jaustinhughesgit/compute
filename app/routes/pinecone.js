8b98d8478a2c56abebdf5fc0171a56618252df03
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-12 19:27:12 -0500

    Pinecone 5

--- app/routes/pinecone.js
+++ app/routes/pinecone.js
@@ -1,28 +1,21 @@
 var express = require('express');
 var router = express.Router();
-import { Pinecone } from '@pinecone-database/pinecone';
+const { Pinecone } = require('@pinecone-database/pinecone');
 
 router.get('/', async function(req, res, next) {
-
     const pinecone = new Pinecone({
         apiKey: process.env.PINECONE_API_KEY,
         environment: process.env.PINECONE_ENVIRONMENT,
-      });
-
-
+    });
 
     try {
         // Connect to your Pinecone index
-        const index = await pinecone.index('categories').describeIndexStats()
-
-        // Perform operations with your index
-        // Example: const queryResult = await index.query(yourQuery);
+        const index = await pinecone.index('categories').describeIndexStats();
 
         // Render your view with Pinecone data
         res.render('pinecone', {
             title: 'Pinecone',
             message: JSON.stringify(index)
-            //, queryResult: queryResult
         });
     } catch (error) {
         // Handle any errors
