a8464b56e0c74545946b93dc2120d49ce9490979
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-23 18:16:49 -0500

    added lib as a param

--- app/scripts/processConfig.js
+++ app/scripts/processConfig.js
@@ -1,12 +1,12 @@
 async function processConfig(config, initialContext, lib) {
     const context = { ...initialContext };
-    for (const [key, value] of Object.entries(config.modules)) {
+    for (const [key, value] of Object.entries(config.modules, lib)) {
         let newPath = await installModule(value, context, lib);
     }
     return context;
 }
 
-async function installModule(moduleName) {
+async function installModule(moduleName, context, lib) {
     const npmConfigArgs = Object.entries({cache: '/tmp/.npm-cache',prefix: '/tmp',}).map(([key, value]) => `--${key}=${value}`).join(' ');
     await lib.exec(`npm install ${moduleName} ${npmConfigArgs}`); 
     return modulePath
