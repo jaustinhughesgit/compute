f86df87ad1f98d533ee53c6db913032674bf6ba6
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-27 11:17:07 -0500

    removed session app.use

--- app/app.js
+++ app/app.js
@@ -3,15 +3,7 @@ const serverless = require('serverless-http');
 const session = require('express-session');
 const passport = require('passport');
 const MicrosoftStrategy = require('passport-microsoft').Strategy;
-
 const app = express();
-
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: true } 
-}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -26,39 +18,25 @@ passport.use(new MicrosoftStrategy({
     scope: ['user.read']
   },
   (accessToken, refreshToken, profile, done) => {
-    // asynchronous verification
-    process.nextTick(() => {
-      return done(null, profile);
-    });
+    process.nextTick(() => {return done(null, profile);});
   }
 ));
 
-// Route to start Microsoft authentication
 app.get('/auth/microsoft',
-  passport.authenticate('microsoft', { scope: ['user.read'] }),
-  (req, res) => {
-    // The request will be redirected to Microsoft for authentication, so this function will not be called.
-  });
+  passport.authenticate('microsoft', { scope: ['user.read'] }),(req, res) => {});
 
-// Microsoft authentication callback
 app.get('/auth/microsoft/callback', 
   passport.authenticate('microsoft', { failureRedirect: '/' }),
-  (req, res) => {
-    // Successful authentication, redirect to dashboard
-    res.redirect('/dashboard');
-  });
+  (req, res) => {res.redirect('/dashboard'); });
 
-// Dashboard route
 app.get('/dashboard', ensureAuthenticated, (req, res) => {
   res.send('<h1>Dashboard</h1><a href="/account">Account</a>');
 });
 
-// Account route
 app.get('/account', ensureAuthenticated, (req, res) => {
   res.send('<h1>Account Details</h1>');
 });
 
-// Function to ensure the user is authenticated
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/');
