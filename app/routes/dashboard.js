7f97311da522a8cf0bfd5b45accc1760da16697f
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-27 17:07:38 -0500

    resetting node_modules

--- app/routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', async function(req, res, next){
-    res.render('dashboard', {title:'1 Var'})
-});
-
-
-module.exports = router;
