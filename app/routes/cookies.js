c3a9df70eda133c50fbcb43af16571ebbbfbf10b
jaustinhughesgit <jaustinhughes@outlook.com>
2023-12-29 12:03:35 -0500

    splitting url and entityDetails

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
 const AWS = require('aws-sdk');
+
+async function getEntity(e, dynamodb){
+    const params = {
+        TableName: 'entities',
+        KeyConditionExpression: 'e = :e',
+        ExpressionAttributeValues: {
+          ':e': subdomainData.Items[0].e
+        }
+      };
+      const entityDetails = await dynamodb.query(params).promise()
+}
+
+
+
 module.exports = function(privateKey, dynamodb, dynamodbLL) {
     var router = express.Router();
     const keyPairId = 'K2LZRHRSYZRU3Y'; 
     const signer = new AWS.CloudFront.Signer(keyPairId, privateKey);
     router.get('/*', async function(req, res, next) {
         const reqPath = req.apiGateway.event.path
-        const fileID = reqPath.split("/")[2]
+        const action = reqPath.split("/")[2]
+        const fileID = reqPath.split("/")[3]
 
         const params = {
             TableName: 'subdomains',
@@ -27,7 +42,10 @@ module.exports = function(privateKey, dynamodb, dynamodbLL) {
           };
           const attributeName = await dynamodb.query(params2).promise()
 
-        console.log("subdomainData",subdomainData)
+          const entityDetails = await getEntity(subdomainData.Items[0].e, dynamodb)
+
+        console.log("entityDetails", entityDetails)
+        console.log("subdomainData", subdomainData)
         console.log("fileID", fileID)
         const expires = 30000;
         const url = "https://public.1var.com/test.json";
@@ -54,7 +72,7 @@ module.exports = function(privateKey, dynamodb, dynamodbLL) {
             for (const cookieName in cookies) {
                 res.cookie(cookieName, cookies[cookieName], { maxAge: expires, httpOnly: true, domain: '.1var.com', secure: true, sameSite: 'None' });
             }
-            res.json({"ok":true,"name":attributeName.Items[0].r});
+            res.json({"ok":true,"entity":{"name":attributeName.Items[0].r}});
         }   
     });
     return router;
