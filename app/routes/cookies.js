eb82941d3c19bb295f2cae836ac9aa4e303c5336
jaustinhughesgit <jaustinhughes@outlook.com>
2024-01-04 14:19:49 -0500

    manual uuid file name

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -585,7 +585,7 @@ module.exports = function(privateKey, dynamodb, dynamodbLL, uuidv4, s3) {
         response = mainObj
 
         const expires = 90000;
-        const url = "https://public.1var.com/test.json"//actions/"+actionFile+".json";
+        const url = "https://public.1var.com/8e70f0d5-b729-49ff-b1f1-761320e63b22.json"//actions/"+actionFile+".json";
         const policy = JSON.stringify({
             Statement: [
                 {
