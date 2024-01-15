02f9ae282a0b8206280a6b6a484589da170d4bfc
jaustinhughesgit <jaustinhughes@outlook.com>
2024-01-14 21:42:05 -0500

    try catch on libs.root.passport.value

--- app/app.js
+++ app/app.js
@@ -286,7 +286,9 @@ async function processString(str, libs, nestedPath) {
     console.log("nestedValue",nestedValue)
     console.log("libs.root", libs.root)
     console.log("libs.root.passport", libs.root.passport)
+    try{
     console.log("typeof libs.root.passport.value", libs.root.passport.value)
+    } catch (err){}
     console.log("typeof nestedValue", typeof nestedValue)
     console.log(" nestedValue[target.key]",  nestedValue[target.key])
     console.log("typeof nestedValue[target.key]", typeof nestedValue[target.key])
