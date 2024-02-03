5096e39c062a51f11e76025923407e752b85a4fe
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-03 03:40:04 -0500

    add 1 day to startUTC

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -904,6 +904,7 @@ async function shiftDaysOfWeekForward(daysOfWeek) {
     let startUTC = await moment.tz(`${startDate} ${startTime}`, "YYYY-MM-DD HH:mm", timeZone).utc();
     let endUTC = await moment.tz(`${startDate} ${endTime}`, "YYYY-MM-DD HH:mm", timeZone).utc();
     if (endUTC.isBefore(startUTC)) {
+      startUTC.add(1, 'day'); 
       endUTC.add(1, 'day'); // Adjusts end time to next day if it ends before it starts (due to time conversion)
     }
   
@@ -1349,6 +1350,7 @@ async function route (req, res, next, privateKey, dynamodb, uuidv4, s3, ses){
                     saturday:sa,
                     sunday:su
                   }
+                  
                   console.log("taskJSON",taskJSON)
                 const schedules = await convertTimespanToUTC(taskJSON)
                 console.log("schedules",schedules)
