8c05dac6a6b801cf813a89355982aa749d911741
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-04 02:20:20 -0500

    rest of the 5 minute rules

--- app/app.js
+++ app/app.js
@@ -158,12 +158,17 @@ app.all("/eb0", async (req, res, next) => {
             console.log(`Error adding target to rule ${ruleName}:`, err);
         }
     }
+
+    function delay(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+    }
     
     // Main function to orchestrate the creation of rules
     async function main() {
-        for (let hour = 0; hour < 1; hour++) {
-            for (let minute = 0; minute < 5; minute += 5) { // Increment minute by 5
+        for (let hour = 1; hour < 24; hour++) {
+            for (let minute = 5; minute < 60; minute += 5) { // Increment minute by 5
                 await createRule(hour, minute);
+                await delay(500);
             }
         }
     }
