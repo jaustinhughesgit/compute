2de156c79ae3a6fd9b7d9b8c6d67d6a3eb7782d9
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-04 23:16:27 -0500

    21 and 22

--- app/app.js
+++ app/app.js
@@ -118,7 +118,7 @@ app.all("/eb0", async (req, res, next) => {
     const client = new SchedulerClient(config);
 
     const createSchedule = async () => {
-        for (let hour = 19; hour < 21; hour++) {
+        for (let hour = 21; hour < 23; hour++) {
             for (let minute = 0; minute < 60; minute += 1) {
                 // Format the hour and minute to ensure two digits
                 const hourFormatted = hour.toString().padStart(2, '0');
