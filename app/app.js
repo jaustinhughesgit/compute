5d127f299193f8505e10b60ec7bbad2082837a0e
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-20 01:20:12 -0500

    dfg

--- app/app.js
+++ app/app.js
@@ -699,7 +699,6 @@ async function processString(str, libs, nestedPath) {
             console.log(isNumber(isArray(arrowJson[1])))
             const isPlaceholder = await isOnePlaceholder(isArray(arrowJson[1]))
             if (isNumber(isArray(arrowJson[1]))){
-                if (isObj2){
                 console.log("THIS ITEM IS AN ARRAY", arrowJson[1])
                 console.log("Array value", isArray(arrowJson[1]))
                 console.log(value)
