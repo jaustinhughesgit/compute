d94e699d801b5ec2f75cb429ed08855b0ca74e34
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-22 00:53:19 -0500

    asdf

--- app/app.js
+++ app/app.js
@@ -1002,6 +1002,7 @@ async function runAction(action, libs, nestedPath, req, res, next){
                     let leftSide1 = await replacePlaceholders(whileCondition[0], libs, nestedPath)
                     let conditionMiddle = whileCondition[1]
                     let rightSide2 = await replacePlaceholders(whileCondition[2], libs, nestedPath)
+                    console.log("$$$", typeof leftSide1, conditionMiddle, typeof rightSide2)
                     console.log("$$$", leftSide1, conditionMiddle, rightSide2)
                         
                         await processAction(action, libs, nestedPath, req, res, next);
