d48c4b065edf8a509bcb4eef023bb9273ad4ffc5
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-21 22:38:30 -0500

    asdf

--- app/app.js
+++ app/app.js
@@ -871,9 +871,9 @@ async function processString(str, libs, nestedPath) {
 
     
     let obj = Object.keys(libs.root).reduce((acc, key) => {
-        //if (!["req", "res"].includes(key)) {
+        if (!["req", "res"].includes(key)) {
           acc[key] = libs.root[key];
-        //}
+        }
         return acc;
       }, {});
 
@@ -888,9 +888,9 @@ async function processString(str, libs, nestedPath) {
     console.log("MMM1", newNestedPath)
     console.log("MMM2", mmm)
     
-    /*if (str == "res"){
+    if (str == "res"){
         mmm = libs.root.context[str].value
-    }*/
+    }
 
     return mmm;
     /*const isExecuted = str.endsWith('}}!');
