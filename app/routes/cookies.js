b7995d59e080505ed549b3f9e0b487095ff52709
jaustinhughesgit <jaustinhughes@outlook.com>
2024-02-24 16:32:42 -0500

    asdf

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -1307,7 +1307,10 @@ async function route (req, res, next, privateKey, dynamodb, uuidv4, s3, ses){
                 const details = await addVersion(e.toString(), "a", a.toString(), null, dynamodb);
                 const result = await createEntity(e.toString(), a.toString(), details.v, eParent.Items[0].g, eParent.Items[0].h, "0", dynamodb);
                 const uniqueId = await getUUID(uuidv4);
-                let subRes = await createSubdomain(uniqueId,a.toString(),e.toString(), "0", isPublic, dynamodb)
+                console.log("newEntityName",newEntityName);
+                console.log("parent", parent);
+                console.log("isPublic", isPublic);
+                let subRes = await createSubdomain(uniqueId,a.toString(),e.toString(), "0", parent.Items[0].z, dynamodb)
                 const fileResult = await createFile(uniqueId, {}, s3);
                 actionFile = uniqueId;
                 const details2 = await addVersion(parent.Items[0].e.toString(), "t", e.toString(), eParent.Items[0].c, dynamodb);
