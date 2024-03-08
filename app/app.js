63f41ad7c15a8f3a24b631f9a24a99048e841b2c
jaustinhughesgit <jaustinhughes@outlook.com>
2024-03-07 20:46:27 -0500

    asdf

--- app/app.js
+++ app/app.js
@@ -562,6 +562,7 @@ async function initializeMiddleware(req, res, next) {
                         req.lib.root.context.math = {"value":math, "context":{}}
                         req.lib.root.context.axios = {"value":axios, "context":{}}
                         req.lib.root.context.fs = {"value":fs, "context":{}}
+                        req.lib.root.context.JSON = {"value":JSON, "context":{}}
                         await initializeModules(req.lib, userJSON, req, res, next);
                         console.log("req.lib.root.context",req.lib.root.context)
                     };
