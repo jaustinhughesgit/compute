d60daa0ed3b130b7ff912538f5f0d15d33c39d4b
jaustinhughesgit <jaustinhughes@outlook.com>
2024-09-26 18:28:29 -0400

    asdf

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -2458,8 +2458,8 @@ async function route(req, res, next, privateKey, dynamodb, uuidv4, s3, ses, open
                 //console.log("to", to)
                 //console.log("ac", ac)
                 if (ex && at && va && to && ac && !buffer) {
-                    console.log("values are truthy",ai.toString() )
                     const ai = await incrementCounterAndGetNewValue('aiCounter', dynamodb);
+                    console.log("values are truthy",ai.toString() )
                     const access = await createAccess(ai.toString(), sub.Items[0].g.toString(), sub.Items[0].e.toString(), ex, at, to, va, ac)
                     console.log(access)
 
