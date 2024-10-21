7c07d375107b9431797dfc21d8a7dd97b5635c18
jaustinhughesgit <jaustinhughes@outlook.com>
2024-10-21 14:06:33 -0400

    asdf

--- app/app.js
+++ app/app.js
@@ -690,11 +690,12 @@ async function installModule(moduleName, contextKey, context, lib) {
         const keys = contextKey.slice(1, -1).split(',').map(key => key.trim());
         for (const key of keys) {
             // Store the export directly
-            context[key] = module[key];
+            console.log("INSTALLING TO VALUE MIGHT NOT WORK")
+            context[key] = {"value":module[key], "context":{}};
         }
     } else {
         // It's a simple module name
-        context[contextKey] = module;
+        context[contextKey] = {"value":module, "context":{}};
     }
     console.log("context", JSON.stringify(context))
 }
