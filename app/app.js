228f069d9a79edafaf5dd8e62cd9a361e61d4fb4
jaustinhughesgit <jaustinhughes@outlook.com>
2024-10-21 21:34:38 -0400

    asdf

--- app/app.js
+++ app/app.js
@@ -1868,7 +1868,7 @@ async function processAction(action, libs, nestedPath, req, res, next) {
                     }
                 }
                 console.log("putValueIntoContext")
-                await putValueIntoContext(firstParts, pathParts, value, libs, index);
+                await putValueIntoContext(firstParts, pathParts, value, libs.root.context, index);
                 console.log("###libs3", libs)
             } else {
                 await addValueToNestedKey(set.key.replace("~/", ""), nestedContext, value);
