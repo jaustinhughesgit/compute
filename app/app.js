329350e8341ea7c1ac335ba018b43c7a0229f9b2
jaustinhughesgit <jaustinhughes@outlook.com>
2024-10-23 15:04:51 -0400

    asdf

--- app/app.js
+++ app/app.js
@@ -2006,7 +2006,7 @@ async function processAction(action, libs, nestedPath, req, res, next) {
             console.log("result2.1",result);
             if (assignExecuted && typeof result === 'function') {
                 console.log("assign executed")
-                result = await result()
+                await result()
             } else if (typeof result === 'function') {
                 result = result;
             } else {
