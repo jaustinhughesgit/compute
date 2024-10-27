c396a376a56482971863c7e36b3479e697f00c97
jaustinhughesgit <jaustinhughes@outlook.com>
2024-10-27 17:32:53 -0400

    asdf

--- app/app.js
+++ app/app.js
@@ -1802,6 +1802,7 @@ async function putValueIntoContext(contextPath, objectPath, value, libs, index) 
         pathHolder[objectPath[objectPath.length - 1]][index] = value
     } else {
         pathHolder[objectPath[objectPath.length - 1]] = value
+        libs.root.context.uploadParams.value.body = value
     }
     console.log("###pathHolder2.3", pathHolder)
 }
