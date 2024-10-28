d45c3f169ca844a0b5139ef41580e610ec2dad9a
jaustinhughesgit <jaustinhughes@outlook.com>
2024-10-27 22:35:58 -0400

    asdf

--- app/app.js
+++ app/app.js
@@ -2439,6 +2439,8 @@ async function applyMethodChain(target, action, libs, nestedPath, assignExecuted
                 console.log("--4--")
                 console.log("result", result)
                 console.log("typeof result", typeof result)
+                console.log("PROMISE")
+                result = await result.promise();
                 // SEE IF WE CAN USE THIS FOR NO METHOD FUNCTIONS LIKE method()(param, param, pram)
             } else if (assignExecuted && typeof result[accessClean] == "function"){
                 console.log("executinig function")
