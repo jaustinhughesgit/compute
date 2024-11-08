a966c325f1c341399c41e1a27225b0f86d09dd8c
jaustinhughesgit <jaustinhughes@outlook.com>
2024-11-08 13:47:34 -0500

    asdf

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -264,7 +264,13 @@ async function verifyThis(fileID, cookie, dynamodb, body) {
                 console.log("deep", deep)
                 if (deep){
                     console.log("inside deep condition")
-                    let usingAuth = await useAuth(Entity, Authenticator, dynamodb)
+                    let splitHost =  body.headers.X-Original-Host.split("/")
+                    console.log("splitHost",splitHost)
+                    let lastHost = splitHost[splitHost.length -1]
+                    console.log("lastHost",lastHost)
+                    let entityClean = lastHost.split("?")[0]
+                    console.log("entityClean",entityClean)
+                    let usingAuth = await useAuth(entityClean, body.headers.X-accessToken, dynamodb)
                     console.log("usingAuth", usingAuth)
                 }
             }
