ca0c0457003225f4f9b40dad1ebe0cfe71402d30
jaustinhughesgit <jaustinhughes@outlook.com>
2024-11-10 07:35:14 -0500

    dfgh

--- app/app.js
+++ app/app.js
@@ -875,6 +875,7 @@ async function initializeMiddleware(req, res, next) {
         console.log("runApp4")
         console.log("req", req)
         console.log("req.body", req.body)
+        console.log("req.headers", req.headers)
         let originalHost = req.headers["X-Original-Host"];
         console.log("originalHost", originalHost)
         let splitOriginalHost = originalHost.split("1var.com")[1]
