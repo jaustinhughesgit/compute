04a5e1d935a3948bcd37843b8ec4ddbd10936abe
jaustinhughesgit <jaustinhughes@outlook.com>
2025-01-11 18:49:49 -0500

    asdf

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -2356,6 +2356,7 @@ async function route(req, res, next, privateKey, dynamodb, uuidv4, s3, ses, open
                 console.log("newJPL");
                 console.log("arrayLogic", arrayLogic);
                 var zeroArray = ["JSON", JSON.stringify(jsonpl)]
+                arrayLogic.unshift(["", "JOIN", "SUBSTITUTE", "MED", "SUM", "AVG", "USE", "CONDITION", "ALL", "ITE", "JSON", "ARRAY", "ADDPROPERTY", "MERGE", "NESTED", "DELETEPROPERTY", "STRING", "INTEGER", "FLOAT", "BUFFER", "APPEND", "PREPEND", "RANGE", "|||", "==", "!=", ">", ">=", "<", "<="]);
                 arrayLogic.unshift(zeroArray)
                 let newJPL = await shorthand(arrayLogic);
                 console.log(newJPL);
