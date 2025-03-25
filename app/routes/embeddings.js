dd1ab5d3c3e6503990f03ac3a0e8d103138096c4
jaustinhughesgit <jaustinhughes@outlook.com>
2025-03-25 19:18:58 -0400

    asdf

--- app/routes/embeddings.js
+++ app/routes/embeddings.js
@@ -23,7 +23,8 @@ router.get('/', async function(req, res, next) {
 
         res.render('embeddings', {
             category: "/event/wedding/people/groom/bachelor-party/itinerary/day-1/arrival/flight-info/individual-arrivals/best-man/arrival-time/4pm",
-            embeddings: JSON.stringify(normalizedEmbedding),
+            embedding: JSON.stringify(embedding),
+            normalized: JSON.stringify(normalizedEmbedding),
         });
     } catch (error) {
         console.error("Error generating embeddings:", error);
