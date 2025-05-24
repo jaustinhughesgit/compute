5299ffa7c65352c68bc9a444758f18968f4f692f
jaustinhughesgit <jaustinhughes@outlook.com>
2025-05-24 00:38:47 -0400

    asdf

--- app/routes/cookies.js
+++ app/routes/cookies.js
@@ -2243,7 +2243,9 @@ async function route(req, res, next, privateKey, dynamodb, uuidv4, s3, ses, open
                 console.log("running app runApp 12345")
                 let ot = await runApp(req, res, next)
                 console.log("ot", ot)
-                return ot.chainParams
+                if (ot){
+                    return ot.chainParams
+                }
             }
             /* else if (action == "transcribe"){
                 mainObj["presign"] = await getPresignedUrl();
