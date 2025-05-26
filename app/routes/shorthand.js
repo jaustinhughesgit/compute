95c82c4b4cc65e21a6f73d961eb61381e04fc026
jaustinhughesgit <jaustinhughes@outlook.com>
2025-05-26 16:34:02 -0400

    asdf

--- app/routes/shorthand.js
+++ app/routes/shorthand.js
@@ -1133,6 +1133,7 @@ async function shorthand(shorthandObj, req, res, next, privateKey, dynamodb, uui
             //console.log("run(skip)2")
             await run(skip);
         }
+        console.log("rowResult",rowResult)
         return rowResult[0];
     }
 
@@ -1218,7 +1219,6 @@ async function shorthand(shorthandObj, req, res, next, privateKey, dynamodb, uui
             console.log("act", act)
             let resp = await route(newReq, res, next, privateKey, dynamodb, uuidv4, s3, ses, openai, Anthropic, dynamodbLL, true, reqPath, reqBody, reqMethod, reqType, reqHeaderSent, signer, act, xAccessToken);
             console.log("ROUTE resp=>", resp);
-            console.log("rowResult",rowResult)
             return resp
         },
         EMPTY: (rowArray) => {
