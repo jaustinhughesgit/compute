459ca73f1e30217e91fa06d528068cda751529fa
jaustinhughesgit <jaustinhughes@outlook.com>
2025-06-09 03:59:17 -0400

    asdf

--- app/app.js
+++ app/app.js
@@ -592,7 +592,7 @@ const entities = {
     console.log("2")
     // ── Extract breadcrumb and body ───────────────────────────
     const [breadcrumb] = Object.keys(singleObject);
-    console.log("3")
+    console.log("3", breadcrumb)
     // ── Create embedding for the body ─────────────────────────
     const { data } = await openai.embeddings.create({
         model: 'text-embedding-3-small',
@@ -600,9 +600,6 @@ const entities = {
     });
     console.log("3.1", data);            // now legal
     const rawEmb   = data[0].embedding;
-    console.log("3.1", data)
-    console.log("3.2", data[0])
-    console.log("3.3",data[0].embedding)
     console.log("4", rawEmb)
     const embedding = toVector(rawEmb);
     console.log("5", embedding)
@@ -636,11 +633,8 @@ const entities = {
     if (dynamoRecord) {
         console.log("10")
       const embKeys = ['emb1', 'emb2', 'emb3', 'emb4', 'emb5'];
-      const vectors = embKeys.map(k => {
-        console.log("k", k);
-        console.log("dynamoRecord",dynamoRecord)
-        toVector(dynamoRecord[k])
-    });
+      const vectors = embKeys.map(k => 
+        toVector(dynamoRecord[k]));
       console.log("11",vectors);
       [dist1, dist2, dist3, dist4, dist5] = vectors.map(vec =>
         vec ? scaledEuclidean(embedding, vec) : null
