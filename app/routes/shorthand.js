02d4055a9563056c3d4f65683618c76f2edc6b9e
jaustinhughesgit <jaustinhughes@outlook.com>
2025-06-19 17:59:21 -0400

    asdf

--- app/routes/shorthand.js
+++ app/routes/shorthand.js
@@ -1813,8 +1813,8 @@ async function shorthand(shorthandObj, req, res, next, privateKey, dynamodb, uui
     console.log("shorthandArray << ^^ >>", shorthandArray)
     let rr0 = await processArray(shorthandArray)
     console.log("rr0", rr0)
-    shorthandObj.content = JSON.parse(JSON.stringify(rr0.content))
-    delete rr0.content;
+    shorthandObj.conclusion = JSON.parse(JSON.stringify(rr0.conclusion))
+    delete rr0.conclusion;
     shorthandObj.published = rr0
     return shorthandObj
 }
