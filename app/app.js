56573c50e39d5f8edc11b74930de7c97a1d29901
jaustinhughesgit <jaustinhughes@outlook.com>
2025-07-11 09:02:41 -0400

    asd

--- app/app.js
+++ app/app.js
@@ -1214,7 +1214,14 @@ async function initializeMiddleware(req, res, next) {
             head = await getHead("su", reqPath.split("/")[1], dynamodb)
             cookie = await manageCookie({}, xAccessToken, res, dynamodb, uuidv4)
             parent = await convertToJSON(head.Items[0].su, [], null, null, cookie, dynamodb, uuidv4, null, null, null, null, dynamodbLL, req.body)
+
+            if (typeof entity.Items[0].z == "string" ){
+                const subByE = await getSub(entity.Items[0].z, "e", dynamodb);
+                console.log("subByE",subByE)
+                fileArray = parent.paths[subByE.Items[0].su];
+            } else {
             fileArray = parent.paths[reqPath.split("/")[1]];
+            }
         }
         let isPublic = head.Items[0].z
 
