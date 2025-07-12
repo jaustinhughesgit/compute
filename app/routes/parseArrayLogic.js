6cca27797a3680b780ce1237a0b13614ac196050
jaustinhughesgit <jaustinhughes@outlook.com>
2025-07-12 16:22:58 -0400

    asdf

--- app/routes/parseArrayLogic.js
+++ app/routes/parseArrayLogic.js
@@ -1591,13 +1591,15 @@ async function parseArrayLogic({ arrayLogic = [], dynamodb, uuidv4, s3, ses, ope
 
             console.log("fixedPossessed",fixedPossessed)
             console.log("typeof fixedPossessed",typeof fixedPossessed)
-            let base = 10000000000000000
+            let base = 10000
             console.log("base", base)
-            let possessedBase = base + fixedPossessed;
+            let converted = fixedPossessed / 1e12;
+            console.log("converted", converted)
+            let possessedBase = base + converted;
             console.log("possessedBase",possessedBase);
-            let domainIndex = parseInt(domains.indexOf(domain) + "00000000000000")
+            let domainIndex = parseInt(domains.indexOf(domain) + "00")
             console.log("domainIndex",domainIndex);
-            let subdomainIndex = parseInt(DOMAIN_SUBS[domain].indexOf(subdomain) + "000000000000")
+            let subdomainIndex = parseInt(DOMAIN_SUBS[domain].indexOf(subdomain))
             console.log("subdomainIndex",subdomainIndex);
             let possessedCombined = possessedBase + domainIndex + subdomainIndex
             console.log("possessedCombined",possessedCombined);
