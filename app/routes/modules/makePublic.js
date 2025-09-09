f1350f9e3101202c68d82b4b7312b49204503bbc
jaustinhughesgit <jaustinhughes@outlook.com>
2025-09-08 20:07:13 -0400

    asdf

--- app/routes/modules/makePublic.js
+++ app/routes/modules/makePublic.js
@@ -1,9 +1,11 @@
 // modules/makePublic.js
+"use strict";
+
 /**
  * Toggles file visibility by moving *all versions* between buckets via updateSubPermission,
  * then returns a fresh convertToJSON of the file.
  */
-module.exports.handle = async function makePublicHandle(ctx) {
+async function makePublicHandle(ctx) {
   const {
     dynamodb, s3, uuidv4, dynamodbLL,
     convertToJSON, updateSubPermission,
@@ -11,12 +13,25 @@ module.exports.handle = async function makePublicHandle(ctx) {
   } = ctx;
 
   // Path: /<anything>/makePublic/:su/:permission
-  const segs = reqPath.split("/");
+  const segs = (reqPath || "").split("/");
   const su = segs[3];
   const permission = segs[4]; // "true" | "false"
 
   await updateSubPermission(su, permission, dynamodb, s3);
 
-  const mainObj = await convertToJSON(su, [], null, null, cookie, dynamodb, uuidv4, null, [], {}, "", dynamodbLL, reqBody);
+  const mainObj = await convertToJSON(
+    su, [], null, null, cookie,
+    dynamodb, uuidv4, null, [], {}, "",
+    dynamodbLL, reqBody
+  );
+
   return { mainObj, actionFile: su };
+}
+
+// Back-compat named export
+module.exports.handle = makePublicHandle;
+
+/** Register hook for the module loader */
+module.exports.register = function register({ on /*, use */ }) {
+  on("makePublic", makePublicHandle);
 };
