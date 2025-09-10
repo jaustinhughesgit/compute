b05068ffb8288ab0f9d92f49151dddf44a0a1b5f
jaustinhughesgit <jaustinhughes@outlook.com>
2025-09-10 16:12:10 -0400

    asdf

--- app/routes/modules/add.js
+++ app/routes/modules/add.js
@@ -192,13 +192,11 @@ function register({ on, use }) {
 
     return {
       ok: true,
-      response: {
-        action: "add",
-        parent: { su: parentSU, e: parentEId, public: parentZ },
-        created: { su: childSU, e: eId, a: aId, name: newEntityName },
-        head: headSU || null,
-        tree, // may be null if convertToJSON signature differs
-      },
+      action: "add",
+      parent: { su: parentSU, e: parentEId, public: parentZ },
+      created: { su: childSU, e: eId, a: aId, name: newEntityName },
+      head: headSU || null,
+      tree, // may be null if convertToJSON signature differs
     };
   });
 
