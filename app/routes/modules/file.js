2b3da1ca0d9bc35da275db566e10409612ae5fe3
jaustinhughesgit <jaustinhughes@outlook.com>
2025-09-11 22:09:20 -0400

    asdf

--- app/routes/modules/file.js
+++ app/routes/modules/file.js
@@ -89,6 +89,7 @@ function register({ on, use }) {
       verifications = await getVerified("gi", cookieGi, dynamodb);
     }
     const splitPath = String(path || "").split("/");
+    console.log("splitPath",splitPath)
     const has1v4r = splitPath.some(seg => seg && seg.startsWith("1v4r"));
     const verified = has1v4r ? await verifyPath(splitPath, verifications, dynamodb) : [];
 
