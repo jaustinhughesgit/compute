74de661547afe8392f8fa1d8de483cd4c0931e17
jaustinhughesgit <jaustinhughes@outlook.com>
2025-09-12 20:33:18 -0400

    asdf

--- app/routes/parseArrayLogic.js
+++ app/routes/parseArrayLogic.js
@@ -1751,6 +1751,7 @@ async function parseArrayLogic({ arrayLogic = [], dynamodb, uuidv4, s3, ses, ope
             const pick = (...xs) => xs.find(s => typeof s === "string" && s.trim());
             const sanitize = s => s.replace(/[\/?#]/g, ' ').trim(); // avoid path chars
 
+            console.log("body>>>>>>>",body)
             const entNameRaw =
                 pick(fixedOutput, body?.input?.name, body?.input?.title, body?.input?.entity) || "untitled";
             const entName = sanitize(entNameRaw);
