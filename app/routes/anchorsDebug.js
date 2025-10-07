a69caf073c493b2d8effaca0e7b00ffd6dc98212
jaustinhughesgit <jaustinhughes@outlook.com>
2025-10-07 05:20:41 -0400

    asdf

--- app/routes/anchorsDebug.js
+++ app/routes/anchorsDebug.js
@@ -1,10 +1,10 @@
-// ./routes/anchors.js
+
+
 var express = require('express');
 var router = express.Router();
 
-// Render the simple test page
-router.get('/', async function(req, res) {
-  res.render('anchorsDebug', {});
+router.get('/', async function(req, res, next){
+    res.render('anchorsDebug', {})
 });
 
 module.exports = router;
